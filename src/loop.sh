b4fecacc2140ace8e3e47020570375e93b4e0ec7
Clive Reavie <clive.reavie@gmail.com>
2025-03-18 10:24:59 +0000

    reverts py & loop.sh

--- src/loop.sh
+++ src/loop.sh
@@ -18,7 +18,7 @@ while : ; do
 	echo "$(date +"%F %T") Sleeping short amount of time out of safety..."
 	sleep 7
 
-	python3 ./octo2influx.py
+	python3 ./octo2influx.py --to_days_ago 1
 
 	echo "$(date +"%F %T") Sleeping $FREQ..."
 	sleep "$FREQ"
